2671a7bec58aa0fe2ba6a17105c4bbf1cf782b56
verial-eth <>
2025-01-18 23:25:03 -0800

    feat(config): Add prettier and format project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettierConfig from 'eslint-config-prettier';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
-    {
-        ignores: [
-            'dist/**',
-            'node_modules/**',
-            '.history/**',
-            'build/**',
-            'coverage/**'
-        ]
-    },
-    eslint.configs.recommended,
-    // NOTE: enabling strict and stylistic rules will override recommended rules
-    // https://typescript-eslint.io/getting-started/#additional-configs
-    // tseslint.configs.recommended,
-    tseslint.configs.strict,
-    tseslint.configs.stylistic,
-    // NOTE: Disable formatting rules to avoid conflicts
-    // https://typescript-eslint.io/users/what-about-formatting
-    prettierConfig,
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      ".history/**",
+      "build/**",
+      "coverage/**",
+    ],
+  },
+  eslint.configs.recommended,
+  // NOTE: enabling strict and stylistic rules will override recommended rules
+  // https://typescript-eslint.io/getting-started/#additional-configs
+  // tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  // NOTE: Disable formatting rules to avoid conflicts
+  // https://typescript-eslint.io/users/what-about-formatting
+  prettierConfig,
 );
